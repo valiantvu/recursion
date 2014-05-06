f6fb841964106b61024f4a9421f37c92b1e186c5
valiantvu <michelle.thi.vu@gmail.com>
2014-05-06 14:33:17 -0700

    Added recursive call for nested objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,7 +5,11 @@
 var stringifyJSON = function(obj) {
 	var stringObj = "{";
 	for (var key in obj){
-		stringObj += "'" + key + "': " + obj[key] + ",";
+		var val = obj[key];
+		if (typeof obj[key] == "object"){
+			val = stringifyJSON(obj[key]);
+		}
+		stringObj += "'" + key + "': " + val + ",";
 	}
 	return stringObj.slice(0, -1) + "}";
 }
