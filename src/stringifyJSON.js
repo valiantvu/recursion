7a4203c6664c836609b89442d441c10d901e3688
valiantvu <michelle.thi.vu@gmail.com>
2014-05-06 19:20:02 -0700

    Fixed extra commas

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,13 +3,43 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-	var stringObj = "{";
-	for (var key in obj){
-		var val = obj[key];
-		if (typeof obj[key] == "object"){
-			val = stringifyJSON(obj[key]);
-		}
-		stringObj += "'" + key + "': " + val + ",";
+	if (typeof obj == "string"){
+		return obj;
 	}
-	return stringObj.slice(0, -1) + "}";
+	else if (obj instanceof Array){
+		var stringObj = "";
+		for (var i = 0; i < obj.length; i++){
+			var val = obj[i];
+			var comma = i == obj.length - 1 ? "" : ",";
+
+			if (val instanceof Array){
+				val = stringifyJSON(val);
+				stringObj += val + comma;
+			}
+			else if (typeof val == "string") {
+				stringObj += "'" + val + "'" + comma;
+			}
+			else if (typeof val == "object"){
+				val = stringifyJSON(val);
+			}
+			else {
+				stringObj += val + comma;
+			}
+		}
+		return "[" + stringObj + "]" + (comma === undefined ? "" : comma);
+	}
+	else if (typeof obj == "object"){
+		var stringObj = "";
+		for (var key in obj){
+			var val = obj[key];
+			if (typeof val == "object"){
+				val = stringifyJSON(val);
+			}
+			stringObj += "'" + key + "':" + val + ",";
+		}
+		return String("{" + stringObj.slice(0, -1) + "}");	
+	}
+	else {
+		return obj;
+	}
 }
