04a4d6f852f1b43eaa29d4da37b222cefe97192b
valiantvu <michelle.thi.vu@gmail.com>
2014-05-06 21:28:25 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,43 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
+	var stringObj = "";
+
 	if (typeof obj == "string"){
-		return obj;
+		stringObj += "\"" + obj + "\"";
 	}
 	else if (obj instanceof Array){
-		var stringObj = "";
 		for (var i = 0; i < obj.length; i++){
 			var val = obj[i];
 			var comma = i == obj.length - 1 ? "" : ",";
 
-			if (val instanceof Array){
-				val = stringifyJSON(val);
-				stringObj += val + comma;
-			}
-			else if (typeof val == "string") {
-				stringObj += "'" + val + "'" + comma;
-			}
-			else if (typeof val == "object"){
-				val = stringifyJSON(val);
-			}
-			else {
-				stringObj += val + comma;
-			}
+			val = stringifyJSON(val);
+			stringObj += val + comma;
 		}
 		return "[" + stringObj + "]" + (comma === undefined ? "" : comma);
 	}
+	else if (typeof obj == "function" || typeof obj == "undefined"){
+	}
+	else if (obj == null){
+		stringObj += null;
+	}
 	else if (typeof obj == "object"){
 		var stringObj = "";
-		for (var key in obj){
-			var val = obj[key];
-			if (typeof val == "object"){
-				val = stringifyJSON(val);
+		for (var key in obj){	
+			val = stringifyJSON(obj[key]);
+			if (val){
+				stringObj += "\"" + key + "\":" + val + ",";
 			}
-			stringObj += "'" + key + "':" + val + ",";
 		}
-		return String("{" + stringObj.slice(0, -1) + "}");	
+		return "{" + stringObj.slice(0, -1) + "}";	
 	}
 	else {
-		return obj;
+		stringObj += obj;
 	}
+	return stringObj;
 }
