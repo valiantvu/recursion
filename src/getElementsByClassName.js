c65a6d6f75764eb0df1a1f950cc53a4f602dbbb1
valiantvu <michelle.thi.vu@gmail.com>
2014-05-07 10:54:25 -0700

    Completed function, specs pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,8 +2,26 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+// You should use document.body, element.childNodes, and element.classList
 // But instead we're going to implement it from scratch:
+// document.body.childNodes[3].classList.contains("targetClassName") ---> true
 var getElementsByClassName = function(className){
-  // your code here
+	var result = [];
+	var element = arguments[1] ? arguments[1] : document;
+	var children = element.childNodes;
+
+	for (var i = 0; i < children.length; i++) {
+		var child = children[i];
+
+		if (child.classList != undefined) {
+			
+			if (child.classList.contains(className)){
+				result.push(child);
+			}
+			if (child.hasChildNodes()) {
+				Array.prototype.push.apply(result,getElementsByClassName(className, child));
+			}	
+		}
+	}
+	return result;
 };
